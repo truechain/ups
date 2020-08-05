b0d038c73afb9fc08a8a6c3b194914c076336e3e
iceming123 <ice_ming@126.com>
2020-08-05 17:38:26 +0800

    upsstore.go: updata upsfile and mgr module

--- upsstore/upsstore.go
+++ upsstore/upsstore.go
@@ -10,14 +10,12 @@ import (
 	"path/filepath"
 	// "math/big"
 	// "time"
-	// "github.com/truechain/ups/accounts"
-	// "github.com/truechain/ups/accounts/keystore"
 	"github.com/truechain/ups/common"
 	// "github.com/truechain/ups/common/hexutil"
 	// "github.com/truechain/ups/common/math"
 	// "github.com/truechain/ups/core"
 	// "github.com/truechain/ups/core/rawdb"
-	// "github.com/truechain/ups/core/types"
+	"github.com/truechain/ups/core/types"
 	// "github.com/truechain/ups/core/vm"
 	// "github.com/truechain/ups/crypto"
 	"github.com/truechain/ups/log"
@@ -69,6 +67,8 @@ type UpsFile struct {
 	hash 	string
 	address common.Address
 	wg 		*sync.WaitGroup
+	cache 	bool 
+	fileHash common.Hash
 }
 func NewUpsFile(name string,addr common.Address,data []byte) *UpsFile {
 	return &UpsFile{
@@ -76,13 +76,30 @@ func NewUpsFile(name string,addr common.Address,data []byte) *UpsFile {
 		address: addr,
 		data:	data,
 		wg:		&sync.WaitGroup{},
+		cache:	false,
 	}
 }
-func (u *UpsFile) Event() {
+func (u *UpsFile) UpdataFileHash() {
+	u.fileHash = types.RlpHash([]interface{}{
+		u.name,
+		u.data,
+	})
+}
+func (u *UpsFile) GetFileHash() common.Hash {
+	if u.fileHash == common.Hash{} {
+		u.UpdateFileHash()
+	}
+	return u.fileHash
+}
+func (u *UpsFile) Finish() {
 	u.wg.Done()
 }
 func (u *UpsFile) Wait() {
 	u.wg.Wait()
+}
+func (u *UpsFile) Event() *UpsFile {
+	u.wg.Add(1)
+	return u
 }
 func (u *UpsFile) getFileNameInCache(cfg *ipfsConfig) string {
 	if cfg == nil {
@@ -94,11 +111,68 @@ func (u *UpsFile) getFileNameInCache(cfg *ipfsConfig) string {
 func (u *UpsFile) setFileHash(hash string) {
 	u.hash = hash
 }
-
-type FileMgr struct {
-	
+func (u *UpsFile) equal(o *UpsFile) bool {
+	return u.GetFileHash() == o.GetFileHash()
+}
+func (u *UpsFile) fileCache(c bool) {
+	u.cache = c
+}
+func (u *UpsFile) baseCopy(o *UpsFile) {
+	u.data,u.name,u.hash = o.data,o.name,o.hash
+	u.cache,u.fileHash = o.cache,o.fileHash
 }
 
+
+type FileMgr struct {
+	caches 	map[common.Hash][]*UpsFile	
+	max 	int
+}
+func NewFileMgr() *FileMgr {
+	return &FileMgr{
+		max:	10,
+		caches: make(map[common.Hash][]*UpsFile),
+	}
+} 
+func GetGlobalFileMgr() *FileMgr {
+	return nil
+}
+func (m *FileMgr) LoadFromCache(cfg *ipfsConfig) error {
+	return nil
+}
+func (m *FileMgr) FileExist(hash common.Hash) bool {
+	return false
+}
+func (m *FileMgr) GetFileByHashCode(hash string) *UpsFile {
+	for _,val := range m.caches {
+		v := val[0]
+		if v.hash == hash {
+			return v
+		}
+	}
+	return nil
+}
+func (m *FileMgr) GetFile(h common.Hash,addr common.Address) (*UpsFile,bool){
+	if val,ok := m.caches[h]; ok {
+		for _,v :=range val {
+			if v.address == addr {
+				return v,true
+			}
+		}
+		return v,false
+	}
+	return nil,false
+}
+func (m *FileMgr) addFile(o *UpsFile) error {
+	if val,ok := m.caches[o.GetFileHash()]; ok {
+		val = append(val,o)
+		m.caches[o.GetFileHash()] = val
+	} else {
+		m.caches[o.GetFileHash()] = []*UpsFile{o}
+	}
+	return nil
+}
+
+/////////////////////////////////////////////////////////////////////////////////
 func cacheFileToHard(cfg *ipfsConfig,file *UpsFile) error {
 	if file == nil {
 		return errors.New("file is nil")
@@ -113,6 +187,7 @@ func cacheFileToHard(cfg *ipfsConfig,file *UpsFile) error {
 	defer func() {
 		bufWriter.Flush()
 		dstFile.Close()
+		file.fileCache(true)
 	}()
 	bufWriter.Write(file.data)
 	return nil
@@ -125,13 +200,6 @@ func removeFileInCache(cfg *ipfsConfig,name string) error {
 	os.Remove(filename)
 	return nil
 }
-func AddFile(file *UpsFile) error {
-	return nil
-}
-func GetFile(name,hash string,addr common.Address) *UpsFile {
-	return nil
-}
-
 func executeUpload(cfg *ipfsConfig,file *UpsFile) error {
 	if cfg == nil {
 		cfg = getDefaultIpfsConfig()
@@ -143,7 +211,7 @@ func executeUpload(cfg *ipfsConfig,file *UpsFile) error {
 		return err
 	}
 	file.setFileHash(cid)
-	file.Event()
+	file.Finish()
 	return nil
 }
 func executeDownload(cfg *ipfsConfig,file *UpsFile) error {
@@ -163,6 +231,35 @@ func executeDownload(cfg *ipfsConfig,file *UpsFile) error {
 		return err
 	}
 	file.data = data
-	file.Event()
+	file.Finish()
 	return nil
 }
+/////////////////////////////////////////////////////////////////////////////////
+func AddFile(file *UpsFile) error {
+	mgr := GetGlobalFileMgr()
+	f1,user := mgr.GetFile(file.GetFileHash())
+	if f1 != nil {
+		file.baseCopy(f1)
+		if !user {
+			mgr.addFile(file)
+		} 
+	} else {
+		// 1. encryption the file
+		// 2. cache the file in the local node
+		// 3. upload the file to ipfs
+		cfg := getDefaultIpfsConfig()
+		if err := cacheFileToHard(cfg,file); err != nil {
+			return err
+		}
+		go func(){
+			executeUpload(cfg,file.Event())
+		}()
+		file.Wait()
+	}
+	return nil
+}
+func GetFile(name,hash string,addr common.Address) *UpsFile {
+	
+	return nil
+}
+
